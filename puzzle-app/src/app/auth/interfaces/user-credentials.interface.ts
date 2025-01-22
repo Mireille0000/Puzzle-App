60c9a590e4236734d667dc05b49e30d08c536c61
Gasanova Tamara <solas93@mail.ru>
2025-01-22 17:33:31 +0300

    feat: implement login service, implement user credentials interface

---
+++ puzzle-app/src/app/auth/interfaces/user-credentials.interface.ts
@@ -0,0 +1,7 @@
+interface UserCredentials {
+  firstName: string | null,
+  lastName: string | null,
+  password: string | null
+}
+
+export default UserCredentials;
--- puzzle-app/src/app/auth/login-page/login-page.component.ts
+++ puzzle-app/src/app/auth/login-page/login-page.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import {
+  Component, inject,
+} from '@angular/core';
 import {
   FormControl, FormGroup, ReactiveFormsModule, Validators,
 } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { isEnglishAlphabet, isCapitalized } from '../custom-validators/name-validators';
+import { isEnglishAlphabet, isCapitalized } from './custom-validators/name-validators';
 import {
   hasDigit, hasLowercaseLetter, hasSpecialCharacter, hasUppercaseLetter,
-} from '../custom-validators/password-validators';
+} from './custom-validators/password-validators';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'pzl-login-page',
@@ -46,6 +49,8 @@ export class LoginPageComponent {
     ),
   });
 
+  loginService: LoginService = inject(LoginService);
+
   get firstName() {
     return this.loginForm.get('firstName');
   }
@@ -60,5 +65,7 @@ export class LoginPageComponent {
 
   onSubmit() {
     console.log(this.loginForm.value);
+    const userCredentials = this.loginForm.value;
+    this.loginService.saveUserCredentials(userCredentials);
   }
 }
