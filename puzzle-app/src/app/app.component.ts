fecf44796790394f9c6a9e264a4e5c2b86bb4ac3
Gasanova Tamara <solas93@mail.ru>
2025-01-23 21:24:59 +0300

    feat: create welcome page component, implement routing functionality

--- puzzle-app/src/app/app.component.ts
+++ puzzle-app/src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LoginPageComponent } from './auth/login-page/login-page.component';
 
 @Component({
   selector: 'pzl-app',
-  imports: [RouterOutlet, LoginPageComponent, ReactiveFormsModule],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'puzzle-app';
 }
--- puzzle-app/src/app/app.routes.ts
+++ puzzle-app/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './auth/login-page/login-page.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'login', component: LoginPageComponent},
+  {path: 'welcome', component: WelcomePageComponent},
+  {path: '', redirectTo: 'welcome', pathMatch: 'full'}
+];
