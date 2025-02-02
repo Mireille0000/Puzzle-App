803d7b4f9a2cf09ace2887a3ee39267b2f46efe6
Gasanova Tamara <solas93@mail.ru>
2025-02-02 19:56:14 +0300

    implement footer component

--- puzzle-app/src/app/app.component.ts
+++ puzzle-app/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
 
 @Component({
   selector: 'pzl-app',
-  imports: [RouterOutlet, ReactiveFormsModule, HeaderComponent],
+  imports: [RouterOutlet, ReactiveFormsModule, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
