aa864fc8ce420b32ab0cba463bca1e2418b9f944
Gasanova Tamara <solas93@mail.ru>
2025-02-04 14:23:49 +0300

    refactor: migrate from background-image to NgOptimizedImage

--- puzzle-app/src/app/app.component.ts
+++ puzzle-app/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './core/header/header.component';
 import { FooterComponent } from './core/footer/footer.component';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'pzl-app',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
