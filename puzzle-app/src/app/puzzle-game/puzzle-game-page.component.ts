add21b1fb33523b5d1cdda006c28c0a848d4d33a
Gasanova Tamara <solas93@mail.ru>
2025-03-05 01:02:47 +0300

    feat: implement check/continue buttons transformation

--- puzzle-app/src/app/puzzle-game/puzzle-game-page.component.ts
+++ puzzle-app/src/app/puzzle-game/puzzle-game-page.component.ts
@@ -5,7 +5,6 @@ import { HintsBlockComponent } from './hints-block/hints-block.component';
 import { PuzzleFieldComponent } from './puzzle-field/puzzle-field.component';
 import { PuzzlesBlockComponent } from './puzzles-block/puzzles-block.component';
 import { PuzzleGameCardsDataService } from './services/puzzle-game-cards-data.service';
-import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-puzzle-game-page',
@@ -38,7 +37,7 @@ export class PuzzleGamePageComponent {
 
   sentencesInRound = '';
 
-  line: string[] = [];
+  test: string[] = []; // to delete
 
 
   ngOnInit(): void {
@@ -54,44 +53,90 @@ export class PuzzleGamePageComponent {
     this.puzzlesDataService.resultPuzzles$.subscribe((data) => {
       this.completedSentence = data;
     })
+    this.puzzlesDataService.sourcePuzzles$.subscribe((data) => {
+      this.sourceWords = data;
+    }) //
 
-    // change a sentence in source block when after clicking on 'check' button if 1) source block is empty
-    // 2) result sentence is equal to currentSentence
+    // 1) check button is disabled if source block is not empty
+    // 2) continue button appears if a sentence is correct
+    // 3) continue <=> check buttons logic
   }
 
-  changeSentence() {
+  isCorrectlyConstucted() {
     const sentence = computed(() => {
       return this.currentSentence().toString();
     })
 
-    if (sentence() === this.completedSentence.join() && this.sentenceNumber() <= 9) {
-      this.sentenceNumber.update((value) => value + 1);
+    if (sentence() === this.completedSentence.join()) {
       this.isCorrect.update(() => true);
+      console.log(this.isCorrect(),'Correct!');
       this.correctSentences.update((value) => {
         value.push(this.currentSentence());
         return value;
       })
-
-      console.log(this.sentenceNumber())
-      this.puzzlesDataService.getCardsData(this.level()).subscribe((data) =>{
-        const card = data.rounds[this.round()].words[this.sentenceNumber()];
-        if (card) {
-          this.puzzlesDataService.getWordsData(this.level(), this.round(), this.sentenceNumber())
-            .subscribe((data) => {
-              this.sourceWords = data;
-            })
-          this.sentencesInRound = card.textExample; // to delete
-        } else {
-          this.isCorrect.update(() => false);
-          console.log(this.isCorrect(), 'New Round');
-        }
-        console.log('Testing', this.sentencesInRound); // to delete
-      }
-      ) //async
-      console.log('Correct', this.isCorrect());
     } else {
       this.isCorrect.update(() => false);
-      console.log(this.isCorrect(), this.sentenceNumber(), 'Try again');
+      console.log(this.isCorrect(),'Try again');
     }
   }
+
+  continue() {
+    this.sentenceNumber.update((value) => value + 1);
+    console.log(this.isCorrect());
+
+    this.puzzlesDataService.getCardsData(this.level()).subscribe((data) =>{
+      const card = data.rounds[this.round()].words[this.sentenceNumber()];
+      if (card) {
+      this.isCorrect.update(() => true);
+      this.puzzlesDataService.getWordsData(this.level(), this.round(), this.sentenceNumber())
+        .subscribe((data) => {
+          this.sourceWords = data;
+        })
+      this.isCorrect.update(() => false);
+      console.log('Continue button', this.isCorrect());
+      } else {
+        console.log(this.isCorrect(), 'New Round');
+      }
+    });
+    this.puzzlesDataService.resultPuzzles$.next([]);
+    this.isCorrect.update(() => false);
+    console.log('Continue button works');
+  }
+
+  // changeSentence() {
+  //   const sentence = computed(() => {
+  //     return this.currentSentence().toString();
+  //   })
+
+  //   if (sentence() === this.completedSentence.join() && this.sentenceNumber() <= 9) {
+  //     this.sentenceNumber.update((value) => value + 1);
+  //     this.isCorrect.update(() => true); // to delete
+  //     this.correctSentences.update((value) => {
+  //       value.push(this.currentSentence());
+  //       return value;
+  //     }) // to delete
+
+  //     console.log(this.sentenceNumber())
+  //     this.puzzlesDataService.getCardsData(this.level()).subscribe((data) =>{
+  //       const card = data.rounds[this.round()].words[this.sentenceNumber()];
+  //       if (card) {
+  //         this.puzzlesDataService.getWordsData(this.level(), this.round(), this.sentenceNumber())
+  //           .subscribe((data) => {
+  //             this.sourceWords = data;
+  //           })
+  //         this.sentencesInRound = card.textExample; // to delete
+  //       } else {
+  //         this.isCorrect.update(() => false);
+  //         console.log(this.isCorrect(), 'New Round');
+  //       }
+  //       console.log('Testing', this.sentencesInRound); // to delete
+  //     }
+  //     ) // useful
+  //     console.log('Correct', this.isCorrect());
+  //   } else {
+  //     // round changing ??
+  //     this.isCorrect.update(() => false); // to delete
+  //     console.log(this.isCorrect(), this.sentenceNumber(), 'Try again'); // delete
+  //   }
+  // }
 }
